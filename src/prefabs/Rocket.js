4294b4497568d570b4f30fd73522e021c807592f
andrea624 <amart434@ucsc.edu>
2025-01-20 17:13:34 -0800

    finished files

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -8,6 +8,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         //tracks rockets firing status
         this.isFiring = false
         this.moveSpeed = 2
+        this.sfxShot = scene.sound.add('sfx-shot')
     }
     update(){
         if (!this.isFiring){
@@ -18,8 +19,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         // fire button
-        if(Phaser.Input.Keyboard.JustDown(keyFIRE)){
+        if(Phaser.Input.Keyboard.JustDown(keyFIRE) && !this.isFiring){
             this.isFiring = true
+            this.sfxShot.play()
         }
         // if fired move up
         if(this.isFiring && this.y >= borderUISize *3 + borderPadding){
@@ -30,5 +32,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.isFiring = false
             this.y = game.config.height - borderUISize - borderPadding
         }
+    }
+    // resets rocket to "ground"
+    reset(){
+        this.isFiring = false
+        this.y = game.config.height - borderUISize - borderPadding
     }
 }
---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,19 @@
+class Spaceship extends Phaser.GameObjects.Sprite{
+    constructor(scene, x, y, texture, frame, pointValue){
+        super(scene, x, y, texture, frame)
+        scene.add.existing(this)
+        this.points = pointValue
+        this.moveSpeed = game.settings.spaceshipSpeed
+    }
+    update(){
+        //moves spaceship left
+        this.x -= this.moveSpeed
+        //wrap from the left to right edge
+        if(this.x <= 0 - this.width){
+            this.x = game.config.width
+        }
+    }
+    reset(){
+        this.x = game.config.width
+    }
+}
